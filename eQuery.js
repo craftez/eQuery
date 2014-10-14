b6f090527e0d5d2bed72d65dfc02ec5cfe7bb468
Eduardo Zamora <eduzambl@gmail.com>
2014-10-14 13:58:05 -0600

    adding and passing tests

--- eQuery.js
+++ eQuery.js
@@ -2,30 +2,119 @@
  * eQuery library
  * @Eduardo Zamora
  */
+(function(global){
 
-var eQuery = function(param) {
-	//If param is a string of type <tag>, create an element of type 'tag'
-	//And store it as element property of the eQuery object
-	var tag = param.replace(/<|>/g,'');
-	this.el = document.createElement(tag);
-};
+	var document = global.document;
+	var $;
 
-//$().html implementation
-eQuery.prototype.html = function(html) {
-	this.el.innerHTML = html;
-	return this;
-};
+	var selfclosing = /^(abbr|br|col|img|input|link|meta|param|hr|area|embed)$/i;
 
-//Override eQuery object toString method, to return the element HTML
-eQuery.prototype.toString = function() {
-	return this.el.outerHTML;
-};
+    // Make sure that self-closing tags are interpreted correctly
+    // Taked from: Secrets of the JavaScript Ninja, John Resig and Bear Bibeault. Chapter 14, Manipulating the DOM
+    var _convert = function (html) {
+        return html.replace(/(<(\w+)[^>]*?)\/>/g, function (all, front, tag) {
+            return selfclosing.test(tag) ? all : front + '></' + tag + '>';
+        });
+    };
 
-//Expose the $ function: $()
-var $ = function(param) {
-	return new eQuery(param);
-};
+	var eQuery = function(param) {
+		var div = document.createElement('div');
+		// Assume that strings that start with < are HTML
+		if(param.charAt(0)==="<") {
+			// If param is a string of type <tag/> or <tag></tag>, create an element of type 'tag'
+            // And store it as element property of the eQuery object
+            div.innerHTML = _convert(param);
+            this.el = div.childNodes[0];
+		} else if (param.charAt(0)==="#"){
+			// Handle $(#id)
+			this.el = document.getElementById(param.replace('#',''));
+		}
+	};
 
-//Note that the eQuery 'toString' method is being called because the 'html' method return
-//the eQuery instance.
+	//$().html implementation
+	eQuery.prototype.html = function(html) {
+		if(html) {
+			this.el.innerHTML = '' + html; // Force the toString method to be executed
+			return this; // Return the dQuery instance to allow chaining 
+		} else {
+			return this.el.innerHTML;
+		}
+	};
 
+	//$().text implementation
+	eQuery.prototype.text = function(text) {
+		if(text) {
+			this.el.textContent = text;
+			return this;  // Return the dQuery instance to allow chaining 
+		} else {
+			return this.el.textContent;
+		}
+	};
+
+	//Override eQuery object toString method, to return the element HTML
+	eQuery.prototype.toString = function() {
+		return this.el.outerHTML;
+	};
+
+	//Expose the $ function: $()
+	global.$ = function(param) {
+		return new eQuery(param);
+	};
+
+	$ = global.$;
+
+	//$.each implementation
+	$.each = function(obj,callback) {
+		var value,
+			i = 0,
+			length = obj.length,
+			isArray = $.isArray( obj );
+
+			if ( isArray ) {
+				for (i; i < length; i++ ) {
+
+					value = callback.call( obj[ i ], i, obj[ i ] );
+
+					if ( value === false ) {
+						break;
+					}
+				}
+			} else {
+				for ( i in obj ) {
+					value = callback.call( obj[ i ], i, obj[ i ] );
+
+					if ( value === false ) {
+						break;
+					}
+				}
+			}
+
+		return obj;
+	};
+
+	//$.inArray implementation
+	$.inArray = function(value, array) {
+		var length = array.length;
+		if (array) {
+			return array.indexOf(value);
+		}
+		return -1;
+	}
+
+	//$.isArray Delegates to ECMAScript5
+	$.isArray = function(obj) {
+		return Array.isArray(obj);
+	}
+
+	//$.isFunction implementation
+	$.isFunction = function(obj) {
+		return !!(obj && obj.constructor && obj.call && obj.apply);
+	}
+
+	//$.isNumber implementation
+	$.isNumeric = function(obj) {
+		return !isNaN(parseFloat(obj)) && isFinite(obj);
+	}
+
+
+})(window);
